1be26582bb5b906fd3d168588fa9f7b2cfc12ebb
KAY S <KS.KWANRUAN@GMAIL.com>
2020-02-19 02:05:10 +1100

    contact done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,32 @@
 import React from "react";
 import { Wrapper, Section, DivContainer } from "../styles/Container";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
+import { FaGithubSquare } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
 
 function Contact(props) {
   return (
     <Wrapper>
       <Section>Contact</Section>
-      <DivContainer> </DivContainer>
+      <DivContainer>
+        <p>
+          <FontAwesomeIcon icon={faEnvelopeSquare} /> &nbsp;
+          <a aria-label="Email Kay" href="mailto:kay.srikaew@icloud.com">
+            kay.srikaew@icloud.com
+          </a>
+        </p>
+        <p>
+          <FaGithubSquare /> &nbsp;
+          <a href="github.com/Kay-bot">github.com/Kay-bot</a>
+        </p>
+        <p>
+          <FaLinkedinIn /> &nbsp;
+          <a href="https://www.linkedin.com/in/kay-srikaew">
+            inkedin.com/in/kay-srikaew
+          </a>
+        </p>
+      </DivContainer>
     </Wrapper>
   );
 }
